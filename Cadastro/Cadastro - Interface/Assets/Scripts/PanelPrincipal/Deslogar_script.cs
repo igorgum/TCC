e19b2ccefb1e92017e7b004b60f3d4c0851bb15f
Igor Duarte <igorfun@hotmail.com>
2017-09-13 18:15:55 -0300

    Botão Deslogar funcional e Campos de Login não podem estar vazios

---
+++ Cadastro/Cadastro - Interface/Assets/Scripts/PanelPrincipal/Deslogar_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deslogar_script : MonoBehaviour {
+	
+	public GameObject painelPrincipal;
+	public GameObject painelLogin;
+	public GameObject painelPergunta;
+
+	public void Sair(){
+		//Procurar zerar todas as variaveis e campos aqui
+
+		//Desligando booleano de loginCerto (Debug)
+
+		//Desligando painéis
+		painelLogin.SetActive (true);
+		painelPrincipal.SetActive(false);
+		painelPergunta.SetActive (false);
+	}
+
+}
--- Cadastro/Cadastro - Interface/ReadMe.txt
+++ Cadastro/Cadastro - Interface/ReadMe.txt
@@ -1,2 +1,5 @@
 Projeto Unity
 
+Versão: Unity 5.6.3f1
+
+Favor não abrir em outra versão para não corromper...
