841ec6e93db483d5ba9b305c2afd2517d4b3af51
Igor Duarte <igorfun@hotmail.com>
2017-10-05 01:02:10 -0300

    Tela de Registro (WIP)

--- Cadastro/Cadastro - Interface/Assets/Scripts/PanelPrincipal/Deslogar_script.cs
+++ Cadastro/Cadastro - Interface/Assets/Scripts/PanelPrincipal/Deslogar_script.cs
@@ -8,15 +8,34 @@ public class Deslogar_script : MonoBehaviour {
 	public GameObject painelPrincipal;
 	public GameObject painelLogin;
 	public GameObject painelPergunta;
-	public InputField login, senha;
+
+	//lista pública de Inputfields para zerar,
+	//incluindo LOGIN e SENHA
+	public InputField[] listaInputfields;
+
+
+	//lista pública de Dropdowns para zerar
+	public Dropdown[] listaDropdowns;
+
 
 	public void Sair(){
-		//Ativando Painel de login p/ referencias os input fields
+
+
+		//zerando Dropdowns
+		foreach (Dropdown i in listaDropdowns)
+		{
+				i.value = 0;
+		}
+
+		//Ativando Painel de login p/ referencias os input fields LOGIN e SENHA
 		painelLogin.SetActive (true);
 
 		//Procurar zerar todas as variaveis e campos aqui
-		login.text = string.Empty;
-		senha.text = string.Empty;
+		//incluindo LOGIN e SENHA
+		foreach (InputField i in listaInputfields)
+		{
+			i.text = string.Empty;
+		}
 
 		//Desligando painéis
 		painelPergunta.SetActive (false);
