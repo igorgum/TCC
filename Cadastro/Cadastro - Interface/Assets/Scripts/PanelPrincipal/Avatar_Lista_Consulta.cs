0f19c6c6dc19612a735fbdaed273e35f87058ddf
Igor Duarte <igorfun@hotmail.com>
2017-10-21 09:31:06 -0200

    sabado, note

---
+++ Cadastro/Cadastro - Interface/Assets/Scripts/PanelPrincipal/Avatar_Lista_Consulta.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar_Lista_Consulta : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
